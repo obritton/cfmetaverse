85b8a845b13c27c62d34178838e8c54b5fd3a177
Ontario Britton <ontario.britton@gmail.com>
2017-11-29 14:31:13 -0500

    Initial content and structure

---
+++ Assets/_Scripts/CfmContentLoader.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CfmContentLoader : MonoBehaviour {
+
+	public void targetFound( string productName ) {
+		print ("CfmContentLoader contentFound: " + productName);
+	}
+}
