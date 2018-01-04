7869e2c135710ae3a9c126c195f292445a0263bf
Ontario Britton <ontario.britton@gmail.com>
2018-01-04 10:46:51 -0500

    Repopulating master with post LP WIP

---
+++ Assets/_Scripts/CfmContentLoader.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CfmContentLoader : MonoBehaviour
+{
+
+	public void targetFound (string productName)
+	{
+		GameObject prefab = (GameObject)Resources.Load ("StructuredContent/" + productName + "/prefab", typeof(GameObject));
+		if (prefab != null) {
+			print (productName + " prefab found!");
+			Instantiate (prefab, transform.position, Quaternion.identity, transform);
+		} else {
+			TextAsset linkAsset = (TextAsset)Resources.Load ("StructuredContent/" + productName + "/link", typeof(TextAsset));
+			if (linkAsset != null) {
+				print (productName + " link found!");
+			} else {
+				TextAsset youtubeAsset = (TextAsset)Resources.Load ("StructuredContent/" + productName + "/youtube", typeof(TextAsset));
+				if (youtubeAsset != null) {
+					print (productName + " youtube found!");
+				} else {
+					TextAsset captionAsset = (TextAsset)Resources.Load ("StructuredContent/" + productName + "/caption", typeof(TextAsset));
+					if (captionAsset != null) {
+						print (productName + " caption found!");
+					}
+				}
+			}
+		}
+	}
+}
---
+++ Assets/_Scripts/CfmTrackableEventHandler.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class CfmTrackableEventHandler : DefaultTrackableEventHandler {
+
+	public CfmContentLoader contentLoader;
+
+	protected override void OnTrackingFound()
+	{
+		base.OnTrackingFound ();
+		contentLoader.targetFound(gameObject.name);
+	}
+}
