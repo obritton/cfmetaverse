45db2d9d1d5794f32baae92a615bc532c4537583
Ontario Britton <ontario.britton@gmail.com>
2018-01-06 11:02:54 -0500

    Lilly pads in

---
+++ Assets/_Scripts/FlowerGrower.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlowerGrower : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Vector3 scale = transform.localScale;
+		transform.localScale = Vector3.one * .00001f;
+		iTween.ScaleTo (gameObject, scale, 13);
+	}
+}
