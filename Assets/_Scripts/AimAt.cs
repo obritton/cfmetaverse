241fdd89f3ac4d25c0c170474716e61b83b4a829
GPU_SYSTEM01\Admin <ken@livingpopups.com>
2018-01-29 11:50:52 -0800

    cover with aim control added

---
+++ Assets/_Scripts/AimAt.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AimAt : MonoBehaviour
+{
+	public GameObject source;
+	public GameObject target;
+	public GameObject clone;
+	public AnimationCurve effectAngle = AnimationCurve.EaseInOut (0, 0.8f, 75, 0);
+	public AnimationCurve effectTimeline = AnimationCurve.EaseInOut (0, 1, 30, 1);
+	public float angleDiff;
+	public float amplitude;
+
+//	void Start ()
+//	{
+//		clone = Instantiate(source.gameObject, source.transform.parent);
+//	}
+
+	void LateUpdate ()
+	{
+		clone.transform.LookAt (target.transform.position);
+		angleDiff = Quaternion.Angle(source.transform.rotation, clone.transform.rotation);
+		amplitude = effectAngle.Evaluate (angleDiff)*effectTimeline.Evaluate (Time.time);
+		source.transform.rotation = Quaternion.Lerp(clone.transform.rotation, source.transform.rotation, 1-amplitude);
+		if(clone.gameObject.GetComponent<Renderer> ())
+			clone.gameObject.GetComponent<Renderer> ().material.color = new Color (1, 1-amplitude, 1);
+	}
+}
---
+++ Assets/_Scripts/AimAtGeneric.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AimAtGeneric : MonoBehaviour
+{
+	[System.Serializable]
+	public class AimAtNode
+	{
+		[Tooltip ("Node should be named yournode_AimAtNode and parented to the joint you want controlled")]
+		public GameObject source;
+		[Tooltip ("Node should be named yournode_Helper and parented to the parent of the joint you want controlled. Disable Mesh Renderer on these nodes after debugging")]
+		public GameObject helper;
+		[Tooltip ("Effective angle range. Time == angle, value == effect. At 0, value should be 1, at 90 (for example), value should be 0 so we get a soft dropoff")]
+		public AnimationCurve effectiveAngle = AnimationCurve.EaseInOut (0, 1, 90, 0);
+		[Tooltip ("Enable/Disable aimAt in time.")]
+		public AnimationCurve effectTimeline = AnimationCurve.EaseInOut (0, 1, 30, 1);
+		public float angleDiff;
+		public float amplitude;
+	}
+
+	public List<AimAtNode> AimAtNodes;
+	public GameObject target;
+
+	void Traverse (GameObject obj)
+	{
+		if (obj.name.ToLower ().Contains ("_aimatnode")) {
+			Debug.Log (obj.name);
+			AimAtNode aan = new AimAtNode ();
+			Transform helperTransform = obj.transform.parent.transform.parent.transform.Find (obj.transform.parent.gameObject.name.ToString () + "_Helper");
+			if (helperTransform == null) {
+				GameObject helper = GameObject.CreatePrimitive (PrimitiveType.Cube);
+				helper.transform.parent = obj.transform.parent.transform.parent;
+				helper.transform.position = obj.transform.parent.position;
+				helper.transform.localScale = new Vector3 (0.025f, 0.05f, 0.25f);
+				helper.name = obj.transform.parent.gameObject.name.ToString () + "_Helper";
+				aan.helper = helper;
+			} else {
+				aan.helper = obj.transform.parent.transform.parent.transform.Find (obj.transform.parent.gameObject.name.ToString () + "_Helper").gameObject;
+				;
+			}
+			aan.source = obj;
+			AimAtNodes.Add (aan);
+		}
+		foreach (Transform child in obj.transform) {
+			Traverse (child.gameObject);
+		}
+	}
+
+	// Execute when added to gameobject or reset is selected
+	void Reset ()
+	{
+		AimAtNodes.Clear ();
+		Traverse (this.gameObject);
+	}
+
+	void LateUpdate ()
+	{
+		foreach (AimAtNode aan in AimAtNodes) {
+			GameObject source = aan.source.transform.parent.gameObject;
+			aan.helper.transform.position = aan.source.transform.parent.position;	// Make sure our helper is on top of the node we want to aim
+			aan.helper.transform.LookAt (target.transform);
+			aan.angleDiff = Quaternion.Angle (source.transform.rotation, aan.helper.transform.rotation);
+			aan.amplitude = aan.effectiveAngle.Evaluate (aan.angleDiff) * aan.effectTimeline.Evaluate (Time.time);
+			source.transform.rotation = Quaternion.Lerp (aan.helper.transform.rotation, source.transform.rotation, 1 - aan.amplitude);
+			if (aan.helper.gameObject.GetComponent<Renderer> ())
+				aan.helper.gameObject.GetComponent<Renderer> ().material.color = new Color (1, 1 - aan.amplitude, 1);
+		}
+	}
+}
